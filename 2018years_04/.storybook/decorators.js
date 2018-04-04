440f32e8057f0134a6d697799edbb8fa68ed7369
“wheatandcat” <wheatandcat@gmail.com>
2018-04-04 23:52:20 +0900

    init

---
+++ 2018years_04/.storybook/decorators.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+
+export const provider = story => (
+  <BrowserRouter>
+    <div>{story()}</div>
+  </BrowserRouter>
+);
+
+export const center = story => (
+  <div
+    style={{
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center"
+    }}
+  >
+    {story()}
+  </div>
+);
